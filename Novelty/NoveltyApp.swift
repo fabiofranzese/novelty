8769b0ec0c862e53402cc3b28a038ab64b4ee62f
Fabio Franzese <fabiofranzeseuni@gmail.com>
2025-05-21 09:56:36 +0200

    Initial Commit

---
+++ Novelty/NoveltyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoveltyApp.swift
+//  Novelty
+//
+//  Created by Fabio on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct NoveltyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
